90f07348fbe560908bf4da0641dcd89ea76b6d14
Teddy-Suh <t3ddy.suh@gmail.com>
2024-10-18 11:10:20 +0900

    feat : 채팅방 초안

--- src/app/chat-list/[id]/chat/ChatRoomContainer.tsx
+++ src/app/chat-list/[id]/chat/ChatRoomContainer.tsx
@@ -1,27 +1,90 @@
-'use client';
+"use client";
 
-import { useEffect, useRef, useState } from 'react';
-import { useForm, Controller } from 'react-hook-form';
+import { useEffect, useRef, useState } from "react";
+import { useForm, Controller } from "react-hook-form";
+import { Client } from "@stomp/stompjs";
+import { useParams } from "next/navigation";
 
 interface ChatFormData {
   message: string;
 }
 
 export default function ChatRoomContainer() {
-  const { control, handleSubmit, reset } = useForm<ChatFormData>();
+  const { id } = useParams();
   const [messages, setMessages] = useState<string[]>([]);
+  const [client, setClient] = useState<Client | null>(null);
+  const { control, handleSubmit, reset } = useForm<ChatFormData>();
+
+  useEffect(() => {
+    // 이전 메세지 받아오기
+    // 이거 없으면 채팅방 나갓다 다시 들어와도 메세지 날라감
+    // const loadPrevMessages = async () => {
+    //   try {
+    //     const response = await axios.get(`${id}`);
+    //     const messages = setMessages(messages);
+    //   } catch (error) {
+    //     console.error("채팅 내역 로드 실패", error);
+    //   }
+    // };
+
+    const client = new Client({
+      // 웹소켓 연결 URL
+      brokerURL: "ws://43.202.152.217:8080/chat", // URL 이거 맞는지....
+      reconnectDelay: 5000,
+      connectHeaders: {},
+    });
+
+    // 연결시 할 일
+    client.onConnect = () => {
+      // 채팅방 구독 (헤더에 엑세스 토큰 필요한가?)
+      // URL 정확히 어떻게 하는지...
+      client.subscribe(`/sub/chat/room/${id}`, (message) => {
+        setMessages((prevMessages) => [...prevMessages, message.body]);
+      });
+    };
+
+    // 연결 끝겼을때 할 일
+    client.onDisconnect = () => {
+      console.log("Disconnected");
+    };
+
+    // 연결 시작
+    client.activate();
+    setClient(client);
+    // 채팅방 벗어나면 연결 종료
+    return () => {
+      if (client) {
+        client.deactivate();
+      }
+    };
+  }, [id]);
+
   const messagesEndRef = useRef<HTMLDivElement | null>(null);
 
+  // 메세지 보내기 (헤더에 엑세스 토큰 필요한가?)
   const onSubmit = (data: ChatFormData) => {
-    if (data.message.trim() === '') {
+    if (data.message.trim() === "") {
       return;
     }
-    setMessages([...messages, data.message]);
+
+    if (client) {
+      const messagePayload = {
+        chatRoomId: id,
+        senderId: "",
+        message: data.message,
+      };
+
+      client.publish({
+        destination: "/pub/chat/message",
+        body: JSON.stringify(messagePayload),
+      });
+    }
+
     reset();
   };
 
   useEffect(() => {
-    messagesEndRef.current?.scrollIntoView({ behavior: 'smooth' });
+    messagesEndRef.current?.scrollIntoView({ behavior: "smooth" });
   }, [messages]);
 
   return (
@@ -30,7 +93,9 @@ export default function ChatRoomContainer() {
       <div className="flex-grow mb-4 space-y-2 overflow-y-auto p-4">
         {messages.map((message, index) => (
           <div key={index} className="chat chat-start">
-            <div className="chat-bubble bg-green-500 text-white rounded-lg p-2 max-w-[80%] break-words">{message}</div>
+            <div className="chat-bubble bg-green-500 text-white rounded-lg p-2 max-w-[80%] break-words">
+              {message}
+            </div>
           </div>
         ))}
         <div ref={messagesEndRef} />
