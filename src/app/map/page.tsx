11ff0be3904761c464f2f18f16a6d96cd0608ad4
Teddy-Suh <t3ddy.suh@gmail.com>
2024-10-18 19:55:24 +0900

    fix : 지도 페이지 떄매 배포 되지 않는거 수정

--- src/app/map/page.tsx
+++ src/app/map/page.tsx
@@ -1,18 +1,19 @@
-// /* eslint-disable @next/next/no-sync-scripts */
+/* eslint-disable @next/next/no-sync-scripts */
 // import MapContainer from "./MapContainer";
 
 // export const metadata = {
 //   title: "Runner's Map",
 // };
 
-// export default function MapPage() {
-//   return (
-//     <>
-//       {/* <script src="https://code.jquery.com/jquery-3.2.1.min.js"></script> */}
-//       <script
-//         src={`https://apis.openapi.sk.com/tmap/jsv2?version=1&appKey=${process.env.NEXT_PUBLIC_TMAP_API_KEY}`}
-//       ></script>
-//       <MapContainer />
-//     </>
-//   );
-// }
+export default function MapPage() {
+  return (
+    <>
+      {/* <script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
+      <script
+        src={`https://apis.openapi.sk.com/tmap/jsv2?version=1&appKey=${process.env.NEXT_PUBLIC_TMAP_API_KEY}`}
+      ></script>
+      <MapContainer /> */}
+      <div>지도임</div>
+    </>
+  );
+}
